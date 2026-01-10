ec4da508eddd767889b5514f344094ff5dfd4946
Fran9676 <40521706+Fran9676@users.noreply.github.com>
2026-01-10 16:12:58 +0100

    a

--- Alpha/Alpha/javascriptCode.js
+++ Alpha/Alpha/javascriptCode.js
@@ -7,19 +7,22 @@ function switchi() {
 		divList[i].classList.toggle(divList[i+1].classList.value);
 		divList[i+1].classList.toggle(divList[i+1].classList.value);
 		divList[i+1].classList.toggle("main");
-		divList[i+1].parentNode.insertBefore(divList[i+1], divList[i].parentNode.firstChild); //Insert (divList[i+1] before firstChild
+		//divList[i].parentNode.appendChild(divList[i]);
+		//divList[i+1].parentNode.insertBefore(divList[i+1], divList[i].parentNode.firstChild); //Insert (divList[i+1] before firstChild
 		i=i+1;
 		console.log(divList);
+		console.log(divList[i]);
 	} else {
 		 console.log(divList[0].parentNode);
 		 divList[i].classList.toggle("main");
 		 divList[i].classList.toggle(divList[0].classList.value);
 		 divList[0].classList.toggle(divList[0].classList.value);
 		 divList[0].classList.toggle("main");
- 		 divList[0].parentNode.insertBefore(divList[0], divList[i].parentNode.firstChild);
+ 		 //divList[i].parentNode.appendChild(divList[i]);
 		 i=0;
 		 console.log(divList);
 	}
+	order();
 }
 
 function controlledSwitch(e){
@@ -29,13 +32,35 @@ function controlledSwitch(e){
 	divList[i].classList.toggle(e.target.parentNode.classList.value);
 	e.target.parentNode.classList.toggle(e.target.parentNode.classList.value);
 	e.target.parentNode.classList.toggle("main");
-	e.target.parentNode.parentNode.insertBefore(e.target.parentNode, divList[i].parentNode.firstChild);
+	order();
 	i = Number(e.target.parentNode.id); // Number() makes a string into an int
-	console.log(divList);
 }
 
 function order() {
-	
+	let x = 0;
+	let y = 0;
+	let calc;
+	let yCalc;
+	divList.forEach(e => {
+		if(x==5){
+			x=0;
+			y=y+1;
+		}
+		console.log(e);
+		if(!(e.classList == "main")){
+			calc = 20*(x);
+			calc = calc+"%"
+			yCalc = 100 + (35*y);
+			yCalc = yCalc+"%";
+			e.style.left = calc;
+			e.style.top = yCalc;
+			console.log(e.style.left);
+		} else {
+			e.style.top = "10%";
+			e.style.left = "10%";
+		}
+		x=x+1;
+	});
 }
 
 function set() {
@@ -43,13 +68,5 @@ function set() {
 	divList[0].classList.toggle(divList[1].classList.value);
 	divList[1].classList.toggle(divList[1].classList.value);
 	divList[1].classList.toggle("main");
-	let y = divList[0].parentNode.childNodes;
-	let x = y[1];
-	console.log(y);
-	console.log(x);
-	divList[0] = divList[1];
-	divList[1] = x;
-	y[1] = divList[1];
-	y[3] = divList[0];
-	console.log(y);
+	order();
 }
